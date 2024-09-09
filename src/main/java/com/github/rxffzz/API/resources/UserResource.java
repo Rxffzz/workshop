cd54bf3efe155832b84f00390866f695d00512ea
Rxffzz <srafael0406@gmail.com>
2024-09-09 07:50:07 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/github/rxffzz/API/resources/UserResource.java
+++ src/main/java/com/github/rxffzz/API/resources/UserResource.java
@@ -1,18 +1,20 @@
 package com.github.rxffzz.API.resources;
 
-import com.github.rxffzz.API.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.rxffzz.API.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "maria", "maria@gmail.com", "99999999", "12323");
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
         return ResponseEntity.ok().body(u);
     }
+
 }
---
+++ src/test/java/com/github/rxffzz/API/config/TestConfigTest.java
@@ -0,0 +1,12 @@
+package com.github.rxffzz.API.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestConfigTest {
+
+    @Test
+    void run() {
+    }
+}
